3b971e03ab36397a7ff0a1222f2e493dd4f0f4f0
Takumi Ito <takumi10194617@gmail.com>
2018-07-19 11:07:55 +0900

    [WIP] 稼働時間制限

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include <M5Stack.h>
-#include <SPI.h>
 #include <MAX31855.h>
+#include <SPI.h>
 
+#define MAX_PROCESS_DURATION 240
 #define THERMO_COUPLE_SCK 18
 #define THERMO_COUPLE_MISO 19
 #define THERMO_COUPLE_MOSI 23
@@ -24,7 +25,10 @@ portMUX_TYPE timeMux = portMUX_INITIALIZER_UNLOCKED;
 
 void setup() {
   M5.begin(true, false);
-  max31855.begin(THERMO_COUPLE_SCK, THERMO_COUPLE_MISO, THERMO_COUPLE_MOSI, THERMO_COUPLE_SLAVE);
+  M5.Speaker.setVolume(1);
+  M5.Speaker.setBeep(440, 300);
+  max31855.begin(THERMO_COUPLE_SCK, THERMO_COUPLE_MISO, THERMO_COUPLE_MOSI,
+                 THERMO_COUPLE_SLAVE);
   // put your setup code here, to run once:
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &OnTimer, true);
@@ -48,10 +52,12 @@ void loop() {
   M5.Lcd.clearDisplay();
   DrawAxis();
   timerAlarmEnable(timer);
+  m5.Speaker.beep();
 
   timerCount = 0;
   while (isProcessing) {
-    if (interruptCounter > 0){
+    m5.update();
+    if (interruptCounter > 0) {
       portENTER_CRITICAL(&timeMux);
       interruptCounter--;
       portEXIT_CRITICAL(&timeMux);
@@ -76,9 +82,13 @@ void IRAM_ATTR OnTimer() {
 void ProcessRoutine() {
   if (max31855.readThermocoupleTemperature() > 0) {
     M5.Lcd.drawString("error!", 0, 0);
+    m5.Speaker.beep();
+    isProcessing = false;
+  } else if (timerCount > MAX_PROCESS_DURATION) {
+    m5.Speaker.beep();
     isProcessing = false;
   } else {
-    M5.Lcd.drawString(String(max31855.thermocoupleTemp, DEC), 50, 0);
+    M5.Lcd.drawString(String(timerCount, DEC) + " / " + String(max31855.thermocoupleTemp, DEC), 50, 0);
     m5.Lcd.drawLine(20 + timerCount, 240 - 20 - (int)max31855.thermocoupleTemp,
                     20 + timerCount, 240 - 20 - (int)max31855.thermocoupleTemp,
                     TFT_BLUE);
